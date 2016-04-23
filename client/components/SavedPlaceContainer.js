1ad615f2d95303a8096f3c046817cbdbadce1605
Sylvia Yanakieva <sylvia.yanakieva@gmail.com>
2016-04-23 14:59:25 -0700

    Add savedPlaceContainer.

--- client/components/SavedPlaceContainer.js
+++ client/components/SavedPlaceContainer.js
@@ -51,3 +51,46 @@ SavedPlaceContainer.propTypes = {
 export default connect(
   mapStateToProps
 )(SavedPlaceContainer);
+
+
+import React, { PropTypes, Component } from 'react';
+import { connect } from 'react-redux';
+import SavedPlaceEntry from './SavedPlaceEntry.js';
+
+class SavedPlaceContainer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    if (this.props.savedPlaces.length === 0) {
+
+      // there are no saved places, so show an empty state
+      return (
+        <div className='col-4-12 saved-places'>
+          <h3>Your Saved Places</h3>
+          <div className='no-saved-places'>
+            <p>
+              If you like a place, click the &hearts; to save it for later.
+            </p>
+          </div>
+        </div>
+      );
+    } else {
+
+      // there are saved places, so display them
+      return (
+        <div className='col-4-12 saved-places'>
+          <h3>Your Saved Places</h3>
+          { this.props.savedPlaces.map((savedPlace) => (
+            <div>
+              <SavedPlaceEntry savedPlace={savedPlace} />
+            </div>
+          ))}
+        </div>
+      );
+    }
+  }
+}
+
+
