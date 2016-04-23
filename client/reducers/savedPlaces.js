36b6265a91cd48ef1cc3716dc743e84aac238ab5
Sylvia Yanakieva <sylvia.yanakieva@gmail.com>
2016-04-23 14:57:24 -0700

    SavedPlaces file fixed.

--- client/reducers/savedPlaces.js
+++ client/reducers/savedPlaces.js
@@ -13,5 +13,16 @@ export default function places (state = initialState, action) {
     return state.concat(action.place);
   default:
     return state;
+  },
+  switch(action.type) { 
+    case types.DELETE_PLACE:
+    for (var i = 0; i < state.length; i++) {
+      if (state[i].googlePlaceId === action.place.googlePlaceId) {
+        return state;
+      }
+    }
+    return state.slice(1, action.place); 
+  default:
+    return state;
   }
 }
